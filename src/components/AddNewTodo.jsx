bad3708ff4481c6244894a895b0097850c8a4b44
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-10-22 19:28:03 +0545

    working on add task popup

--- src/components/AddNewTodo.jsx
+++ src/components/AddNewTodo.jsx
@@ -1,12 +1,68 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
+import { Bell, CalendarDay, Clock, Palette, X } from "react-bootstrap-icons";
 function AddNewTodo() {
   const [showModal, setShowModal] = useState(false);
+  const [text, setText] = useState("");
+  const [day, setDay] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+
+  console.log(text);
   return (
     <div className="AddNewTodo">
-      <button>+ New Todo</button>
+      <div className="btn">
+        <button onClick={() => setShowModal(true)}>+ New Todo</button>
+      </div>
       <Modal showModal={showModal} setShowModal={setShowModal}>
-        <div>Hello Dear!!!</div>
+        <form>
+          <div className="text">
+            <h3>Add new to do!</h3>
+            <input
+              type="text"
+              placeholder="Enter your todo"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              autoFocus
+            />
+          </div>
+          <div className="remind">
+            <Bell />
+            <p>Remind Me</p>
+          </div>
+
+          <div className="pick-day">
+            <div className="title">
+              <CalendarDay />
+              <p>Choose a day</p>
+            </div>
+            date-picker
+          </div>
+
+          <div className="pick-time">
+            <div className="title">
+              <Clock />
+              <p>Choose a time</p>
+            </div>
+            Clock-picker
+          </div>
+
+          <div className="pick-project">
+            <div className="title">
+              <Palette />
+              <p>Choose a project</p>
+            </div>
+            <div className="projects">
+              <div className="project active">Personal</div>
+              <div className="project">Work</div>
+            </div>
+          </div>
+          <div className="cancel" onClick={() => setShowModal(false)}>
+            <X size={40} />
+          </div>
+          <div className="confirm">
+            <button>+ Add to do </button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
