8efb00562e88420543d7dc23e64f3e30bc5c2f80
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-10-23 19:23:38 +0545

    timepicker added

--- src/components/AddNewTodo.jsx
+++ src/components/AddNewTodo.jsx
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 import Modal from "./Modal";
 import { Bell, CalendarDay, Clock, Palette, X } from "react-bootstrap-icons";
 import DatePicker from "react-datepicker";
-import TimePicker from "react-time-picker";
-import "react-time-picker/dist/TimePicker.css";
-import "react-clock/dist/Clock.css";
+import { TimePicker } from "react-ios-time-picker";
 import "react-datepicker/dist/react-datepicker.css";
 function AddNewTodo() {
   const [showModal, setShowModal] = useState(false);
   const [text, setText] = useState("");
   const [day, setDay] = useState(new Date());
-  const [time, setTime] = useState(new Date());
 
-  console.log(text);
+  // Initialize time with the current time
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+  const formattedHour = currentHour < 10 ? `0${currentHour}` : `${currentHour}`;
+  const formattedMinute =
+    currentMinute < 10 ? `0${currentMinute}` : `${currentMinute}`;
+  const initialTime = `${formattedHour}:${formattedMinute}`;
+  const [time, setTime] = useState(initialTime);
+
   return (
     <div className="AddNewTodo">
       <div className="btn">
@@ -48,12 +54,7 @@ function AddNewTodo() {
               <Clock />
               <p>Choose a time</p>
             </div>
-            <DatePicker
-              selected={time}
-              onChange={(time) => setTime(time)}
-              showTimeSelect
-              dateFormat="Pp"
-            />
+            <TimePicker value={time} onChange={(time) => setTime(time)} />
           </div>
 
           <div className="pick-project">
