5816b2cb6fcb1a0a093e4e96bb4aa70ade1693ca
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-10-25 10:32:25 +0545

    project section edited

--- src/components/AddNewProject.jsx
+++ src/components/AddNewProject.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Plus } from "react-bootstrap-icons";
 
 function AddNewProject() {
-  return <div>AddNewProjects</div>;
+  return (
+    <div className="AddNewProject">
+      <div className="add-button">
+        <span>
+          <Plus size="20" />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default AddNewProject;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import RenameProjects from "./RenameProjects";
-function Project() {
+import { Pencil, XCircle } from "react-bootstrap-icons";
+function Project({ project, edit }) {
   return (
-    <div>
-      <RenameProjects />
+    <div className="Project">
+      <div className="name">{project.name}</div>
+      <div className="btns">
+        {edit ? (
+          <div className="edit-delete">
+            <span className="edit">
+              <Pencil size="13" />
+            </span>
+            <span className="delete">
+              <XCircle size="13" />
+            </span>
+          </div>
+        ) : project.numOfTodos === 0 ? (
+          ""
+        ) : (
+          <div className="total-todos">{project.numOfTodos}</div>
+        )}
+      </div>
     </div>
   );
 }
