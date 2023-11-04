42895751545b5072e8876b14e9868e9186642379
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-11-04 23:39:13 +0545

    working to setup firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPWXfbbwyk71OuXHsu2KMgBfYMBMR9zGY",
+  authDomain: "todo-app-d0eb6.firebaseapp.com",
+  projectId: "todo-app-d0eb6",
+  storageBucket: "todo-app-d0eb6.appspot.com",
+  messagingSenderId: "46362406531",
+  appId: "1:46362406531:web:b3a1e554d57f3fd541b5b6",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
