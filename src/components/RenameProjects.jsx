233813be35f4cacf676764db804893902bb7fb69
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-11-11 12:31:57 +0545

    rename features added

--- src/components/RenameProjects.jsx
+++ src/components/RenameProjects.jsx
@@ -1,12 +1,53 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import ProjectForm from "./ProjectForm";
+import { TodoContext } from "../context";
 import db from "../firebase";
+import {
+  collection,
+  updateDoc,
+  query,
+  getDocs,
+  doc,
+  where,
+} from "firebase/firestore";
 
 function RenameProjects({ project, setShowModel }) {
+  // STATE
   const [newProjectName, setNewProjectName] = useState(project.name);
+  // CONTEXT
+  const { selectedProject, setSelectedProject } = useContext(TodoContext);
 
-  const renameProject = (project, newProjectName) => {
-    const projectRef = collection();
+  const renameProject = async (project, newProjectName) => {
+    const projectRef = collection(db, "projects");
+    const todoRef = collection(db, "todos");
+    const { name: oldProjectName } = project;
+
+    const projectQuery = query(projectRef, where("name", "==", newProjectName));
+    const todoQuery = query(
+      todoRef,
+      where("projectName", "==", oldProjectName)
+    );
+    try {
+      const projectSnapshot = await getDocs(projectQuery);
+      if (!projectSnapshot.empty) {
+        alert("Project with the same name already exists!!!");
+      } else {
+        const projectId = doc(db, "projects", project.id);
+        await updateDoc(projectId, { name: newProjectName });
+
+        const todoSnapshot = await getDocs(todoQuery);
+        todoSnapshot.forEach(async (doc) => {
+          await updateDoc(doc.ref, { projectName: newProjectName });
+        });
+
+        setShowModel(false);
+      }
+      if (selectedProject === oldProjectName) {
+        setSelectedProject(newProjectName);
+      }
+    } catch (error) {
+      console.log("Error updating project document: ", error);
+    }
   };
 
   function handleSubmit(e) {
