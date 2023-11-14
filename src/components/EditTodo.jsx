7109688fcbc9703a99dad3c26815e99dd1773b00
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-11-14 20:49:20 +0545

    edit todo constructed

--- src/components/EditTodo.jsx
+++ src/components/EditTodo.jsx
@@ -2,12 +2,15 @@ import React, { useContext, useEffect, useState } from "react";
 import TodoForm from "./TodoForm";
 import { TodoContext } from "../context";
 import moment from "moment";
+import { doc, updateDoc } from "firebase/firestore";
+import db from "../firebase";
+
 function EditTodo() {
   // STATE
   const [text, setText] = useState("");
   const [day, setDay] = useState(new Date());
   const [time, setTime] = useState(new Date());
-  const [todoProject, setTodoProject] = useState();
+  const [todoProject, setTodoProject] = useState("");
 
   // CONTEXT
   const { selectedTodo, projects } = useContext(TodoContext);
@@ -15,11 +18,28 @@ function EditTodo() {
   useEffect(() => {
     if (selectedTodo) {
       setText(selectedTodo.text);
-      setDay(moment(selectedTodo.date, "MM/DD/YYYY"));
-      // setTime(moment(selectedTodo.time, "hh:mm A"));
+      if (selectedTodo.date) {
+        setDay(moment(selectedTodo.date, "MM/DD/YYYY").toDate());
+      }
+      if (selectedTodo.time) {
+        setTime(moment(selectedTodo.time, "hh:mm A").toDate());
+      }
       setTodoProject(selectedTodo.projectName);
     }
   }, [selectedTodo]);
+
+  useEffect(() => {
+    if (selectedTodo && text && day && time && todoProject !== undefined) {
+      const todoRef = doc(db, "todos", selectedTodo.id);
+      updateDoc(todoRef, {
+        text,
+        date: moment(day).format("MM/DD/YYYY"),
+        day: moment(day).format("d"),
+        time: moment(time).format("hh:mm A"),
+        projectName: todoProject,
+      });
+    }
+  }, [text, day, time, todoProject, selectedTodo]);
 
   function handleSubmit(e) {}
 
